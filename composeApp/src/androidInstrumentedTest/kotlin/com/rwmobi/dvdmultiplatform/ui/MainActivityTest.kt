bc1fb8cb4e06dc8735b3008793518fcba9f4eaf5
Ryan W <ryan@rwmobi.com>
2024-08-11 13:54:06 +0100

    [test] AndroidTest: setup dependencies and empty test

---
+++ composeApp/src/androidInstrumentedTest/kotlin/com/rwmobi/dvdmultiplatform/ui/MainActivityTest.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ * Sponsored by RW MobiMedia UK Limited
+ *
+ */
+
+package com.rwmobi.dvdmultiplatform.ui
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.rwmobi.dvdmultiplatform.MainActivity
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@kotlinx.coroutines.ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private lateinit var mainActivityTestRobot: MainActivityTestRobot
+
+    @Before
+    fun setUp() {
+        mainActivityTestRobot = MainActivityTestRobot(composeTestRule)
+    }
+
+    @Test
+    fun appLaunchSuccessfully() {
+        with(mainActivityTestRobot) {
+        }
+    }
+}
---
+++ composeApp/src/androidInstrumentedTest/kotlin/com/rwmobi/dvdmultiplatform/ui/test/ComposePagerTestRule.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024. Ryan Wong
+ * https://github.com/ryanw-mobile
+ * Sponsored by RW MobiMedia UK Limited
+ *
+ */
+
+package com.rwmobi.dvdmultiplatform.ui.test
+
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.rwmobi.dvdmultiplatform.MainActivity
+
+typealias ComposePagerTestRule = AndroidComposeTestRule<ActivityScenarioRule<MainActivity>, MainActivity>
